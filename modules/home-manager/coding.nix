12ca844dcfe59f5a61bddfaf2845217bbb104567
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2026-04-18 01:05:49 +0200

    mcp + gitconfig
    - i kinda have a gitconfig with some crap in it; take what matters and format it as nix expects it (i pasted someone else config) in @file:coding.nix
    /Users/astahmer/.gitconfig; tell me what you removed/didnt take
    
    - add a mcp json with the nix mcp for vscode/copilot kinda like its done for opencode here /Users/astahmer/dev/alex/nixfiles/.references/manu-nixfiles/opencode.jsonc

--- modules/home-manager/coding.nix
+++ modules/home-manager/coding.nix
@@ -18,9 +18,7 @@
         vscode
         fnm
         jujutsu
-        neovim
         curl
-        ripgrep
         uv
         qdirstat
         htop
@@ -28,27 +26,19 @@
         docker-compose
       ];
 
-      # ------------------------------------------------------------------------
-      # Shell Configuration
-      # ------------------------------------------------------------------------
       programs.bash.enable = true;
       programs.bash.shellAliases = {
-        nixos-switch = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/.nixfiles#pc-fixe";
+        nixos-switch = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/dev/alex/nixfiles#${config.flake.nixosHostName}";
       };
       programs.bash.initExtra = ''
         eval "$(${pkgs.lib.getExe pkgs.fnm} env --use-on-cd --shell bash)"
       '';
 
-      # ------------------------------------------------------------------------
-      # Git Configuration
-      # ------------------------------------------------------------------------
-      # `lib.generators.toGitINI` cannot express both `[color] branch = auto` and `[color "branch"]`
-      # in one attrset, so the three `[color "..."]` blocks live in `includes` (raw snippet).
       programs.git = {
         enable = true;
         package = pkgs.git;
+        lfs.enable = true;
         ignores = [
-          ".vscode/tasks.json"
           ".DS_Store"
           ".DS_Store?"
           "._*"
@@ -64,111 +54,40 @@
           "*.tmp"
           "*.temp"
         ];
-        includes = [
-          {
-            path = pkgs.writeText "git-color-subsections.ini" ''
-              [color "branch"]
-              	current = yellow reverse
-              	local = yellow
-              	remote = green
-
-              [color "diff"]
-              	meta = yellow bold
-              	frag = magenta bold
-              	old = red bold
-              	new = green bold
-
-              [color "status"]
-              	added = yellow
-              	changed = green
-              	untracked = cyan
-            '';
-          }
-        ];
         settings = {
           user = {
-            name = "Vincent-HD";
-            email = "vincenthoudan@gmail.com";
+            name = "Alexandre Stahmer";
+            email = "alexandre.stahmer@gmail.com";
           };
           core = {
             editor = "code --wait";
-            autocrlf = "input";
-            quotepath = false;
-            pager = "less -FRX";
             excludesFile = "${config.home.homeDirectory}/.config/git/ignore";
           };
           init.defaultBranch = "main";
           pull.rebase = true;
           push = {
-            default = "simple";
             followTags = true;
-          };
-          merge = {
-            conflictstyle = "diff3";
-            tool = "code";
-          };
-          mergetool = {
-            code = {
-              cmd = "code --wait $MERGED";
-            };
-          };
-          diff = {
-            tool = "code";
-            algorithm = "histogram";
-            renames = "copies";
-          };
-          difftool = {
-            code = {
-              cmd = "code --wait --diff $LOCAL $REMOTE";
-            };
+            autoSetupRemote = true;
           };
           rebase = {
             autoStash = true;
-            autoSquash = true;
+            updateRefs = true;
           };
-          fetch.prune = true;
-          branch = {
-            autoSetupMerge = "always";
-            autoSetupRebase = "always";
-          };
-          color = {
-            ui = "auto";
-            branch = "auto";
-            diff = "auto";
-            status = "auto";
-          };
-          alias = {
-            st = "status";
-            co = "checkout";
-            br = "branch";
-            ci = "commit";
-            unstage = "reset HEAD --";
-            last = "log -1 HEAD";
-            lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-            ll = "log --oneline --graph --decorate --all";
-            amend = "commit --amend --no-edit";
-            fix = "commit --fixup";
-            squash = "commit --squash";
-            wip = "commit -am \"WIP\"";
-            undo = "reset HEAD~1 --mixed";
-            stash-show = "stash show -p";
-            find = "!git log --pretty=\"format:%Cgreen%H %Cblue%s\" --name-status --grep";
-            filelog = "log -u";
-            aliases = "config --get-regexp alias";
-          };
-          help.autocorrect = 1;
           rerere.enabled = true;
-          log.date = "relative";
-          grep.lineNumber = true;
-          tag.sort = "version:refname";
-          versionsort.suffix = [
-            "-pre"
-            ".pre"
-            "-beta"
-            ".beta"
-            "-rc"
-            ".rc"
-          ];
+          diff.algorithm = "histogram";
+          branch.sort = "-committerdate";
+          tag.sort = "taggerdate";
+          pager.diff = false;
+          alias = {
+            go = "checkout";
+            prev = "checkout -";
+            new = "checkout -b";
+            delete = "branch -D";
+            rename = "branch -m";
+            hist = ''log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'';
+            undo = "reset HEAD~ --mixed";
+            search = ''!git rev-list --all | xargs git grep -F'';
+          };
         };
       };
 
