bf452035656df7013b96761350dc7a9a018fe7c2
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2026-04-22 18:26:34 +0200

    single ryu module

---
+++ modules/ryu.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+{
+  config.flake.modules.homeManager.ryu =
+    { pkgs, lib, ... }:
+    let
+      ryu = pkgs.rustPlatform.buildRustPackage {
+        pname = "jj-ryu";
+        version = "0.0.1-alpha.11";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "dmmulroy";
+          repo = "jj-ryu";
+          rev = "main";
+          sha256 = "0vp3kc5h9mdi3kfwyzi8467mdr4lwkiikm6hb10b9n42mjz2akl0";
+        };
+
+        cargoHash = "sha256-OD1DpV4s6tgOnDEAfJWScdSKqtYArbqIJVClOtUCYa4=";
+        doCheck = false;
+
+        meta = {
+          description = "Stacked PRs for Jujutsu (jj-ryu)";
+          license = pkgs.lib.licenses.mit;
+          homepage = "https://github.com/dmmulroy/jj-ryu";
+        };
+      };
+    in {
+      home.packages = [ ryu ];
+    };
+
+  config.flake.modules.nixos.ryu =
+    { pkgs, lib, ... }:
+    let
+      ryu = pkgs.rustPlatform.buildRustPackage {
+        pname = "jj-ryu";
+        version = "0.0.1-alpha.11";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "dmmulroy";
+          repo = "jj-ryu";
+          rev = "main";
+          sha256 = "0vp3kc5h9mdi3kfwyzi8467mdr4lwkiikm6hb10b9n42mjz2akl0";
+        };
+
+        cargoHash = "sha256-OD1DpV4s6tgOnDEAfJWScdSKqtYArbqIJVClOtUCYa4=";
+        doCheck = false;
+
+        meta = {
+          description = "Stacked PRs for Jujutsu (jj-ryu)";
+          license = pkgs.lib.licenses.mit;
+          homepage = "https://github.com/dmmulroy/jj-ryu";
+        };
+      };
+    in {
+      environment.systemPackages = [ ryu ];
+    };
+}
