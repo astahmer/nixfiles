42b932005398fba72e3b45b04cf13d1267d62f5f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2026-04-26 11:21:23 +0200

    rm vscode global settings apply

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,28 +1,8 @@
 { ... }:
 {
   config.flake.modules.homeManager.vscode =
-    { lib, pkgs, ... }:
-    let
-      settingsJson = builtins.readFile ../assets/vscode/settings.jsonc;
-      settingsWithNixd = builtins.replaceStrings
-        [ "\"nix.serverPath\": \"nixd\"" ]
-        [ "\"nix.serverPath\": \"${lib.getExe pkgs.nixd}\"" ]
-        settingsJson;
-      settingsWithNixfmt = builtins.replaceStrings
-        [ "\"nixfmt\"" ]
-        [ "\"${lib.getExe pkgs.nixfmt}\"" ]
-        settingsWithNixd;
-    in
+    { pkgs, ... }:
     {
       home.packages = [ pkgs.vscode ];
-
-      home.file = lib.mkMerge [
-        (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-          "Library/Application Support/Code/User/settings.json".text = settingsWithNixfmt;
-        })
-        (lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-          ".config/Code/User/settings.json".text = settingsWithNixfmt;
-        })
-      ];
     };
 }
